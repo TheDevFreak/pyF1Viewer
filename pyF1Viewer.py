2b5f7cccb5cab1d9a9d0e571e2168069c0ebcd6d
TheDevFreak <thedevfreak@gmail.com>
2021-03-13 10:46:58 +0000

    Make pylint happier

--- pyF1Viewer.py
+++ pyF1Viewer.py
@@ -1,3 +1,4 @@
+"""pyF1Viewer - F1TV interface in Python"""
 from subprocess import call
 import json
 import re
@@ -7,65 +8,69 @@ import requests
 
 
 class F1TVApp:
+    """App class for F1TV"""
+
     def __init__(self):
         """Initialising F1TVApp"""
         self.f1api = "https://api.formula1.com/v2/"
         self.f1tvapi = "https://f1tv.formula1.com/2.0/R/ENG/BIG_SCREEN_HLS/"
-        self.apiKey = None
+        self.api_key = None
         self.headers = {"User-Agent": "RaceControl"}
         self.ascendontoken = None
-        self.f1tvapi_groupId = 14
+        self.f1tvapi_group_id = 14
         while True:
             self.mainpage()
 
     def get_api_key(self):
         """Obtain F1 account api api key"""
         # Download script from f1tv site
-        r = requests.get("https://account.formula1.com/scripts/main.min.js")
+        f1_account_script_data = requests.get(
+            "https://account.formula1.com/scripts/main.min.js"
+        )
         # Use regex to extract apikey
-        self.apiKey = re.findall('apikey: *"(.*?)"', r.text)[0]
+        self.api_key = re.findall('apikey: *"(.*?)"', f1_account_script_data.text)[0]
 
     def login(self, username, password):
         """Logs into F1TV, stores logins for 23hours to reduce auth calls"""
 
         # Mostly unrelated, but we must get the users "groupId" and store it - makes little difference but can't hurt
         try:
-            self.f1tvapi_groupId = requests.get(
+            self.f1tvapi_group_id = requests.get(
                 "https://f1tv.formula1.com/1.0/R/ENG/BIG_SCREEN_HLS/ALL/USER/LOCATION"
             ).json()["resultObj"]["userLocation"][0]["groupId"]
-        except:
+        except KeyError:
             pass
 
         login_headers = {
             "Accept": "application/json, text/javascript, */*; q=0.01",
             "User-Agent": "RaceControl",
-            "apiKey": self.apiKey,
+            "api_key": self.api_key,
             "Content-Type": "application/json",
         }
         data = {"Login": username, "Password": password}
         # Check if we have already stored auth
         if os.path.isfile("auth.json"):
             # Check if credentials are within the last 24 hours
-            with open("auth.json") as f:
-                saved_auth = json.load(f)
+            with open("auth.json") as auth_file:
+                saved_auth = json.load(auth_file)
             # Check within 23 hours to be safe
             if saved_auth["time"] <= int(time.time()) - 82800:
                 # We need to get new auth:
 
                 print("Retrieving and storing new auth")
-                r = requests.post(
+                auth_data = requests.post(
                     f"{self.f1api}account/subscriber/authenticate/by-password",
                     headers=login_headers,
                     json=data,
                 )
                 # return the ascendontoken
-                self.ascendontoken = r.json()["data"]["subscriptionToken"]
+                self.ascendontoken = auth_data.json()["data"]["subscriptionToken"]
                 file_data = {
                     "time": int(time.time()),
-                    "token": r.json()["data"]["subscriptionToken"],
+                    "token": auth_data.json()["data"]["subscriptionToken"],
                 }
-                with open("auth.json", "w") as f:
-                    json.dump(file_data, f)
+                with open("auth.json", "w") as auth_file:
+                    json.dump(file_data, auth_file)
 
             else:
                 # Our existing token is new-enough
@@ -74,36 +79,39 @@ class F1TVApp:
         else:
             # We don't have any auth and so therefore must create & save it.
             print("Retrieving and storing new auth")
-            r = requests.post(
+            auth_data = requests.post(
                 f"{self.f1api}account/subscriber/authenticate/by-password",
                 headers=login_headers,
                 json=data,
             )
             # return the ascendontoken
-            self.ascendontoken = r.json()["data"]["subscriptionToken"]
+            self.ascendontoken = auth_data.json()["data"]["subscriptionToken"]
             file_data = {
                 "time": int(time.time()),
-                "token": r.json()["data"]["subscriptionToken"],
+                "token": auth_data.json()["data"]["subscriptionToken"],
             }
-            with open("auth.json", "w") as f:
-                json.dump(file_data, f)
+            with open("auth.json", "w") as auth_file:
+                json.dump(file_data, auth_file)
 
-    def play_content(self, contentId, channelId=None):
-        """With a given contentId and optional channelId, play that by printing the url and launching mpv"""
+    def play_content(self, content_id, channel_id=None):
+        """With a given content_id and optional channel_id, play that by printing the url and launching mpv"""
         url = "https://f1tv.formula1.com/1.0/R/ENG/BIG_SCREEN_HLS/ALL/CONTENT/PLAY"
-        params = {"contentId": contentId}
-        if channelId:
-            params["channelId"] = channelId
+        params = {"content_id": content_id}
+        if channel_id:
+            params["channel_id"] = channel_id
         content_headers = {**self.headers, **{"ascendontoken": self.ascendontoken}}
 
-        r = requests.get(url, params=params, headers=content_headers)
-        if r.ok:
-            print("Launching mpv with stream url: " + r.json()["resultObj"]["url"])
-            call(f"mpv \"{r.json()['resultObj']['url']}\"")
+        content_m3u8_request = requests.get(url, params=params, headers=content_headers)
+        if content_m3u8_request.ok:
+            print(
+                "Launching mpv with stream url: "
+                + content_m3u8_request.json()["resultObj"]["url"]
+            )
+            call(f"mpv \"{content_m3u8_request.json()['resultObj']['url']}\"")
 
-    def check_additional_streams(self, contentId):
+    def check_additional_streams(self, content_id):
         """Method to check if contendId has additional streams (IE: Onboards, PLC, Data)"""
-        url = f"{self.f1tvapi}ALL/CONTENT/VIDEO/{contentId}/F1_TV_Pro_Monthly/{self.f1tvapi_groupId}"
+        url = f"{self.f1tvapi}ALL/CONTENT/VIDEO/{content_id}/F1_TV_Pro_Monthly/{self.f1tvapi_group_id}"
         content_data = requests.get(url).json()
 
         if (
@@ -111,20 +119,20 @@ class F1TVApp:
             in content_data["resultObj"]["containers"][0]["metadata"]
         ):
             # There are some additional streams - print them out and give a choice
-            print(f"1. {contentId}/nochannel - Main Feed")
+            print(f"1. {content_id}/nochannel - Main Feed")
             counter = 2
             for additional_stream in content_data["resultObj"]["containers"][0][
                 "metadata"
             ]["additionalStreams"]:
                 # Get channel id
-                channelId = (
+                channel_id = (
                     additional_stream["playbackUrl"]
                     .split("CONTENT/PLAY?")[1]
                     .split("&")[0]
                     .split("=")[1]
                 )
                 print(
-                    f"{counter}. {contentId}/{channelId} - {additional_stream['type']} - {additional_stream['title']}"
+                    f"{counter}. {content_id}/{channel_id} - {additional_stream['type']} - {additional_stream['title']}"
                 )
                 counter += 1
             # Take a user input and decrement by 2 if not 1
@@ -132,7 +140,7 @@ class F1TVApp:
             if user_input != 1:
                 user_input -= 2
                 self.play_content(
-                    contentId,
+                    content_id,
                     content_data["resultObj"]["containers"][0]["metadata"][
                         "additionalStreams"
                     ][user_input]["playbackUrl"]
@@ -141,14 +149,14 @@ class F1TVApp:
                     .split("=")[1],
                 )
             else:
-                self.play_content(contentId)
+                self.play_content(content_id)
         else:
-            self.play_content(contentId)
+            self.play_content(content_id)
 
     def meeting_content(self, meetingid):
         """Get contents of a meeting (contents=session meeting=event)"""
         meeting_data = requests.get(
-            f"{self.f1tvapi}ALL/PAGE/SANDWICH/F1_TV_Pro_Monthly/{self.f1tvapi_groupId}?meetingId={meetingid}&title=weekend-sessions"
+            f"{self.f1tvapi}ALL/PAGE/SANDWICH/F1_TV_Pro_Monthly/{self.f1tvapi_group_id}?meetingId={meetingid}&title=weekend-sessions"
         )
         # Print out all sessions of meeting
         if meeting_data.ok:
@@ -166,7 +174,7 @@ class F1TVApp:
     def year_content(self, year):
         """Get all content for a given year"""
         year_data = requests.get(
-            f"{self.f1tvapi}ALL/PAGE/SEARCH/VOD/F1_TV_Pro_Monthly/{self.f1tvapi_groupId}?filter_objectSubtype=Meeting&filter_season={year}&filter_fetchAll=Y&filter_orderByFom=Y"
+            f"{self.f1tvapi}ALL/PAGE/SEARCH/VOD/F1_TV_Pro_Monthly/{self.f1tvapi_group_id}?filter_objectSubtype=Meeting&filter_season={year}&filter_fetchAll=Y&filter_orderByFom=Y"
         )
         # Print out all "meetings"
         if year_data.ok:
@@ -185,11 +193,9 @@ class F1TVApp:
             )
 
     # Archive Related Methods
-    def archive_year(self, pageId):
+    def archive_year(self, page_id):
         """Individual years from archive, used by archive_year_block"""
-        url = (
-            f"{self.f1tvapi}ALL/PAGE/{pageId}/F1_TV_Pro_Monthly/{self.f1tvapi_groupId}"
-        )
+        url = f"{self.f1tvapi}ALL/PAGE/{page_id}/F1_TV_Pro_Monthly/{self.f1tvapi_group_id}"
         archive_year_data = requests.get(url).json()
         # Build menu for year's different categories
         counter = 1
@@ -210,43 +216,43 @@ class F1TVApp:
         # Take input and decrement by 1 to get the right one.
         previous_user_input = user_input
         user_input = int(input("Choice> ")) - 1
-        contentId = archive_year_data["resultObj"]["containers"][previous_user_input][
+        content_id = archive_year_data["resultObj"]["containers"][previous_user_input][
             "retrieveItems"
         ]["resultObj"]["containers"][user_input]["id"]
-        self.check_additional_streams(contentId)
+        self.check_additional_streams(content_id)
 
-    def archive_year_block(self, collectionId, type="EXTCOLLECTION"):
+    def archive_year_block(self, collection_id, access_type="EXTCOLLECTION"):
         """Used as part of the shows/archive/documentaries feature; 
         this takes "blocks" (horizontal scrollers in the UI) of the pages and spits out their data"""
-        if type != "EXTCOLLECTION":
-            url = f"{self.f1tvapi}ALL/PAGE/SEARCH/VOD/F1_TV_Pro_Monthly/{self.f1tvapi_groupId}"
-            archive_years = requests.get(url, params=collectionId).json()
+        if access_type != "EXTCOLLECTION":
+            url = f"{self.f1tvapi}ALL/PAGE/SEARCH/VOD/F1_TV_Pro_Monthly/{self.f1tvapi_group_id}"
+            archive_years = requests.get(url, params=collection_id).json()
         else:
-            url = f"{self.f1tvapi}ALL/PAGE/EXTCOLLECTION/{collectionId}/F1_TV_Pro_Monthly/{self.f1tvapi_groupId}"
+            url = f"{self.f1tvapi}ALL/PAGE/EXTCOLLECTION/{collection_id}/F1_TV_Pro_Monthly/{self.f1tvapi_group_id}"
             archive_years = requests.get(url).json()
         # Build menu for archive block's years
         counter = 1
         for year in archive_years["resultObj"]["containers"]:
-            if type == "SEARCH":
+            if access_type == "SEARCH":
                 print(f"{counter}. {year['metadata']['title']}")
             else:
                 try:
                     print(f"{counter}. {year['metadata']['season']}")
-                except:
+                except KeyError:
                     print(f"{counter}. {year['metadata']['title']}")
             counter += 1
         # Take input and decrement by 1 to get the right one.
         user_input = int(input("Choice> ")) - 1
         try:
-            pageId = (
+            page_id = (
                 archive_years["resultObj"]["containers"][user_input]["actions"][0][
                     "uri"
                 ]
                 .split("ALL/PAGE/")[1]
                 .split("/")[0]
             )
-            self.archive_year(pageId)
-        except:
+            self.archive_year(page_id)
+        except (KeyError, IndexError):
             # If we've hit this it's probably just directly a link to a season review
             self.check_additional_streams(
                 archive_years["resultObj"]["containers"][user_input]["id"]
@@ -255,38 +261,38 @@ class F1TVApp:
     def archive(self):
         """Function allows accessing the f1tv api for the "Archive" page"""
         archive_data = requests.get(
-            f"{self.f1tvapi}ALL/PAGE/493/F1_TV_Pro_Monthly/{self.f1tvapi_groupId}"
+            f"{self.f1tvapi}ALL/PAGE/493/F1_TV_Pro_Monthly/{self.f1tvapi_group_id}"
         ).json()
 
         # Print out all archive blocks and give users a choice
         counter = 1
         for container in archive_data["resultObj"]["containers"]:
             if (
-                container["metadata"]["label"] != None
+                container["metadata"]["label"] is not None
                 and len(container["retrieveItems"]["resultObj"]) > 0
             ):
                 print(f"{counter}. {container['metadata']['label']}")
             counter += 1
         # Take input and decrement by 1 to get the right one.
         user_input = int(input("Choice> ")) - 1
-        collectionId = archive_data["resultObj"]["containers"][user_input][
+        collection_id = archive_data["resultObj"]["containers"][user_input][
             "retrieveItems"
         ]["uriOriginal"].split("/TRAY/EXTCOLLECTION/")[1]
-        self.archive_year_block(collectionId)
+        self.archive_year_block(collection_id)
 
     # "Shows"/"Documentaries" Related Functions
 
-    def shows_documentaries(self, pageId):
+    def shows_documentaries(self, page_id):
         """Function allows accessing the f1tv api for the "shows" or "documentaries" pages"""
         shows_data = requests.get(
-            f"{self.f1tvapi}ALL/PAGE/{pageId}/F1_TV_Pro_Monthly/{self.f1tvapi_groupId}"
+            f"{self.f1tvapi}ALL/PAGE/{page_id}/F1_TV_Pro_Monthly/{self.f1tvapi_group_id}"
         ).json()
 
         # Print out all archive blocks and give users a choice
         counter = 1
         for container in shows_data["resultObj"]["containers"]:
             if (
-                container["metadata"]["label"] != None
+                container["metadata"]["label"] is not None
                 and len(container["retrieveItems"]["resultObj"]) > 0
             ):
                 print(f"{counter}. {container['metadata']['label']}")
@@ -297,18 +303,18 @@ class F1TVApp:
                     for item in container["retrieveItems"]["resultObj"]["containers"]:
                         combined += item["metadata"]["title"] + ", "
                     combined = combined[:-2]
-                except:
+                except (KeyError, IndexError):
                     combined = "None"
                 print(f"{counter}. {combined}")
             counter += 1
         # Take input and decrement by 1 to get the right one.
         user_input = int(input("Choice> ")) - 1
         try:
-            collectionId = shows_data["resultObj"]["containers"][user_input][
+            collection_id = shows_data["resultObj"]["containers"][user_input][
                 "retrieveItems"
             ]["uriOriginal"].split("/TRAY/EXTCOLLECTION/")[1]
-            self.archive_year_block(collectionId)
-        except:
+            self.archive_year_block(collection_id)
+        except (KeyError, IndexError):
             # Since that didn't work we need to handle this differently
             # Use archive_year_block with some hackers
             params = {}
@@ -325,17 +331,17 @@ class F1TVApp:
             self.archive_year_block(params, "SEARCH")
 
     def mainpage(self):
+        """Mainpage for F1TV Interface"""
         menu_items = ["Login", "Year Choice", "Archive", "Shows", "Documentaries"]
         # If there is a current live session - put it on the main menu
         frontpage_url = (
-            f"{self.f1tvapi}ALL/PAGE/395/F1_TV_Pro_Monthly/{self.f1tvapi_groupId}"
+            f"{self.f1tvapi}ALL/PAGE/395/F1_TV_Pro_Monthly/{self.f1tvapi_group_id}"
         )
         frontpage_data = requests.get(frontpage_url).json()
         for item in frontpage_data["resultObj"]["containers"]:
             for sub_item in item["retrieveItems"]["resultObj"]["containers"]:
                 if sub_item["metadata"]["contentSubtype"] == "LIVE":
                     # This is (one of?) the currently live event(s)!
-                    live_event = sub_item["id"]
                     menu_items.insert(
                         1,
                         f"{sub_item['id']} - LIVE EVENT - {sub_item['metadata']['title']}",
